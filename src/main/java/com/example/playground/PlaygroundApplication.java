7536449882c1d9535655e9be2a1769f0af8ade28
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-04-02 22:12:32 +0900

    Introduce explaining variables

--- src/main/java/com/example/playground/PlaygroundApplication.java
+++ src/main/java/com/example/playground/PlaygroundApplication.java
@@ -8,8 +8,26 @@ public class PlaygroundApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlaygroundApplication.class, args);
-		System.out.println("Hello World.");
 
+		PlaygroundApplication a = new PlaygroundApplication();
+		a.sub();
+	}
+
+	public  void sub() {
+		int quantity =  1;
+		int unitPrice = 500;
+		int basePrice = quantity * unitPrice;
+
+		int shippingCost = 0;
+		if(basePrice < 3000)
+			shippingCost = 500;
+
+		double itemPrice = (basePrice + shippingCost) * takeRate();
+		System.out.println(itemPrice);
+	}
+
+	public  double takeRate()  {
+		return 1.10;
 	}
 
 }
