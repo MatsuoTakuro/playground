7d2a57722716eb258731279e6f4de4a4551db1f3
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-04-03 11:04:59 +0900

    Create Quantity class that handles correct quantities

---
+++ src/main/java/com/example/playground/Quantity.java
@@ -0,0 +1,32 @@
+package com.example.playground;
+
+public class Quantity {
+    static final  int MIN = 1;
+    static final int MAX = 100;
+
+    int value;
+
+    Quantity(int value) {
+        if (value < MIN) throw new
+                IllegalArgumentException("illegal: " + "less than " + MIN);
+        if (value > MAX) throw new
+                IllegalArgumentException("illegal: " + "more than " + MAX);
+        this.value = value;
+    }
+
+    Quantity add(Quantity other) {
+        if (!canAdd(other)) throw new
+                IllegalArgumentException("illegal: " + "total is more than " + MAX);
+        int addedTotal = addValue(other);
+        return new Quantity(addedTotal);
+    }
+
+    boolean canAdd(Quantity other) {
+        int added = addValue(other);
+        return added <= MAX;
+    }
+
+    private int addValue(Quantity other) {
+        return this.value + other.value;
+    }
+}
